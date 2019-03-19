1bc9ba1fa4e211e9647431962562b2f0df4bdf2c
Dexter Marks-Barber <dexter.mb@outlook.com>
2019-03-19 13:40:23 +0000

    Check if the model is pageable or not

--- src/Sources/XeroQuerySource.php
+++ src/Sources/XeroQuerySource.php
@@ -4,141 +4,178 @@ namespace LangleyFoxall\uxdm\Sources;
 use DivineOmega\uxdm\Interfaces\SourceInterface;
 use LangleyFoxall\uxdm\Traits\HasFilters;
 use LangleyFoxall\uxdm\Traits\HasMaps;
+use XeroPHP\Remote\Model;
 use XeroPHP\Remote\Query;
 
 class XeroQuerySource implements SourceInterface
 {
-	use HasFilters, HasMaps;
+    use HasFilters, HasMaps;
 
-	/** @var Query $query */
-	protected $query;
+    /** @var Query $query */
+    protected $query;
 
-	/** @var XeroCollectionSource $collectionSource */
-	protected $collectionSource;
+    /** @var XeroCollectionSource $collectionSource */
+    protected $collectionSource;
 
-	/**
-	 * @param Query $query
-	 */
-	public function __construct(Query $query)
-	{
-		$this->query = $query;
-	}
+    /** @var bool $pageable */
+    protected $pageable;
 
-	/**
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return XeroCollectionSource
-	 */
-	public function collection()
-	{
-		$this->bootstrapIfNotSet();
+    /**
+     * @param Query $query
+     */
+    public function __construct(Query $query)
+    {
+        $this->query = $query;
 
-		return $this->collectionSource;
-	}
+        $this->pageable(
+            $query->getFrom()
+        );
+    }
 
-	/**
-	 * @param \Closure $closure
-	 *
-	 * @return $this
-	 */
-	public function query(\Closure $closure)
-	{
-		$out = $closure($this->query);
+    /**
+     * @throws \XeroPHP\Remote\Exception
+     * @return XeroCollectionSource
+     */
+    public function collection()
+    {
+        $this->bootstrapIfNotSet();
 
-		if ($out instanceof Query) {
-			$this->query = $out;
-		}
+        return $this->collectionSource;
+    }
 
-		return $this;
-	}
+    /**
+     * @param \Closure $closure
+     *
+     * @return $this
+     */
+    public function query(\Closure $closure)
+    {
+        $out = $closure($this->query);
 
-	/**
-	 * @param int   $page
-	 * @param array $fieldsToRetrieve
-	 *
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return array
-	 */
-	public function getDataRows($page = 1, $fieldsToRetrieve = [])
-	{
-		try {
+        if ($out instanceof Query) {
+            $this->query = $out;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param bool|string $pageable
+     * @return $this|bool
+     */
+    public function pageable($pageable = null)
+    {
+        if (is_bool($pageable)) {
+            $this->pageable = $pageable;
+
+            return $this;
+        }
+
+        if (is_string($pageable) || is_object($pageable)) {
+            $validClass = class_exists($pageable) && is_subclass_of($pageable, Model::class);
+            $hasMethod = method_exists($pageable, 'isPageable');
+
+            if ($validClass && $hasMethod) {
+                $this->pageable = (new $pageable)->isPageable();
+            }
+
+            return $this;
+        }
+
+        return $this->pageable;
+    }
+
+    /**
+     * @param int $page
+     * @param array $fieldsToRetrieve
+     *
+     * @throws \XeroPHP\Remote\Exception
+     * @return array
+     */
+    public function getDataRows($page = 1, $fieldsToRetrieve = [])
+    {
+        try {
             $this->bootstrap($page);
 
             return $this->collectionSource->getDataRows(
                 1, $fieldsToRetrieve
             );
         } catch (\Exception $e) {
-		    return [];
+            return [];
         }
-	}
+    }
 
-	/**
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return int
-	 */
-	public function countDataRows()
-	{
-		$this->bootstrapIfNotSet();
+    /**
+     * @throws \XeroPHP\Remote\Exception
+     * @return int
+     */
+    public function countDataRows()
+    {
+        $this->bootstrapIfNotSet();
 
-		return $this->collectionSource->countDataRows();
-	}
+        return $this->collectionSource->countDataRows();
+    }
 
-	/**
-	 * @return int
-	 */
-	public function countPages()
-	{
-		return 1;
-	}
+    /**
+     * @return int
+     */
+    public function countPages()
+    {
+        return 1;
+    }
 
-	/**
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return array|string[]
-	 */
-	public function getFields()
-	{
-		$this->bootstrapIfNotSet();
+    /**
+     * @throws \XeroPHP\Remote\Exception
+     * @return array|string[]
+     */
+    public function getFields()
+    {
+        $this->bootstrapIfNotSet();
 
-		return $this->collectionSource->getFields();
-	}
+        return $this->collectionSource->getFields();
+    }
 
-	/**
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return void
-	 */
-	private function bootstrapIfNotSet()
-	{
-		if (!$this->collectionSource) {
-			$this->bootstrap();
-		}
-	}
+    /**
+     * @throws \XeroPHP\Remote\Exception
+     * @return void
+     */
+    private function bootstrapIfNotSet()
+    {
+        if (!$this->collectionSource) {
+            $this->bootstrap();
+        }
+    }
 
-	/**
-	 * @param int $page
-	 *
-	 * @throws \XeroPHP\Remote\Exception
-	 * @return void
-	 */
-	private function bootstrap($page = 1)
-	{
-		$collection = $this->query->page($page)->execute();
+    /**
+     * @param int $page
+     *
+     * @throws \XeroPHP\Remote\Exception
+     * @return void
+     */
+    private function bootstrap($page = 1)
+    {
+        $collection =
+            $this->pageable
+                ? $this->query->page($page)->execute()
+                : $this->query->execute();
 
-		$this->collectionSource =
-			new XeroCollectionSource($collection);
+        $this->collectionSource =
+            new XeroCollectionSource($collection);
 
-		$this->collectionSource->perPage(
-			count($collection)
-		);
+        $this->collectionSource->perPage(
+            count($collection)
+        );
 
-		if (!empty($this->dataRowFilters)) {
-			$this->collection()->mergeDataRowFilters(
-				$this->dataRowFilters
-			);
-		}
+        if (!empty($this->dataRowFilters)) {
+            $this->collection()->mergeDataRowFilters(
+                $this->dataRowFilters
+            );
+        }
 
-		if (!empty($this->dataRowMaps)) {
-			$this->collection()->mergeDataRowMaps(
-				$this->dataRowMaps
-			);
-		}
-	}
+        if (!empty($this->dataRowMaps)) {
+            $this->collection()->mergeDataRowMaps(
+                $this->dataRowMaps
+            );
+        }
+    }
 }
